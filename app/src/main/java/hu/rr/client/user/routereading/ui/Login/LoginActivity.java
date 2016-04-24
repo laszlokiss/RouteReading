38e90c624d37fe82d82a84b3827c3ddd8525e100
laszlokiss <laszlo.kiss9203@gmail.com>
2016-04-25 00:23:43 +0200

    Login feature

---
+++ app/src/main/java/hu/rr/client/user/routereading/ui/Login/LoginActivity.java
@@ -0,0 +1,76 @@
+package hu.rr.client.user.routereading.ui.Login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+
+import javax.inject.Inject;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import hu.rr.client.user.routereading.R;
+import hu.rr.client.user.routereading.RouteReadingApplication;
+import hu.rr.client.user.routereading.ui.projects.ProjectsActivity;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity implements LoginScreen {
+
+    @Inject
+    LoginPresenter loginPresenter;
+
+    @Bind(R.id.login_progress)
+    ProgressBar loginProgress;
+    @Bind(R.id.email)
+    AutoCompleteTextView email;
+    @Bind(R.id.password)
+    EditText password;
+    @Bind(R.id.email_sign_in_button)
+    Button emailSignInButton;
+    @Bind(R.id.email_login_form)
+    LinearLayout emailLoginForm;
+    @Bind(R.id.login_form)
+    ScrollView loginForm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        RouteReadingApplication.injector.inject(this);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loginPresenter.attachScreen(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        loginPresenter.detachScreen();
+    }
+
+    @Override
+    public void navigateToProjectsList() {
+        Intent intent = new Intent(this, ProjectsActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.email_sign_in_button)
+    public void onClick() {
+        loginPresenter.login(email.getText().toString(), password.getText().toString());
+    }
+}
+
