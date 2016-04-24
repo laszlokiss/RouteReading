8b814a77cde98fe796ed0427a12d7acbbd094f2d
laszlokiss <laszlo.kiss9203@gmail.com>
2016-04-25 00:56:14 +0200

    Hotfix

--- app/src/main/java/hu/rr/client/user/routereading/ui/UIModule.java
+++ app/src/main/java/hu/rr/client/user/routereading/ui/UIModule.java
@@ -10,6 +10,7 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import hu.rr.client.user.routereading.di.Network;
+import hu.rr.client.user.routereading.ui.Login.LoginPresenter;
 
 /**
  * Created by Laci on 2016.04.19..
@@ -35,4 +36,9 @@ public class UIModule {
         return Executors.newFixedThreadPool(1);
     }
 
+    @Provides
+    @Singleton
+    public LoginPresenter provideLoginPresenter() {
+        return new LoginPresenter();
+    }
 }
